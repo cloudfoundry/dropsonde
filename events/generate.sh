11a35855bf617b1ca6910a45bb05621fa2490634
Alex Jackson <ajackson@pivotallabs.com>
2014-03-27 09:09:40 -0600

    envelope for emitted events
    Signed-off-by: Johannes Petzold <jpetzold@pivotallabs.com>

--- events/generate.sh
+++ events/generate.sh
@@ -4,4 +4,4 @@ set -e
 
 go install code.google.com/p/gogoprotobuf/protoc-gen-gogo
 
-protoc --plugin=$GOPATH/bin/protoc-gen-gogo --gogo_out=. http.proto
+protoc --plugin=$GOPATH/bin/protoc-gen-gogo --gogo_out=. *.proto
