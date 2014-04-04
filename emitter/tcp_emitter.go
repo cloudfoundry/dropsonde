a9542bf77c626853fe18030afaee3ad0e50e66e4
Johannes Tuchscherer <jtuchscherer@pivotallabs.com>
2014-04-04 14:05:18 -0600

    Added TcpEmitter.
    Signed-off-by: Johannes Petzold <jpetzold@pivotallabs.com>

---
+++ emitter/tcp_emitter.go
@@ -0,0 +1,48 @@
+package emitter
+
+import (
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/cloudfoundry-incubator/dropsonde/events"
+	"net"
+)
+
+type tcpEmitter struct {
+	tcpAddr *net.TCPAddr
+	origin  *events.Origin
+}
+
+func NewTcpEmitter(remoteAddress string) (e Emitter, err error) {
+	tcpAddr, err := net.ResolveTCPAddr("tcp", remoteAddress)
+	if err != nil {
+		return
+	}
+
+	e = &tcpEmitter{tcpAddr: tcpAddr}
+	return
+}
+
+func (e *tcpEmitter) SetOrigin(origin *events.Origin) {
+	e.origin = origin
+}
+
+func (e *tcpEmitter) Emit(event events.Event) (err error) {
+	envelope, err := Wrap(event, e.origin)
+	if err != nil {
+		return
+	}
+	data, err := proto.Marshal(envelope)
+	if err != nil {
+		return
+	}
+	tcpConn, err := net.DialTCP("tcp", nil, e.tcpAddr)
+	if err != nil {
+		return
+	}
+	defer tcpConn.Close()
+
+	_, err = tcpConn.Write(data)
+	return
+}
+
+func (e *tcpEmitter) Close() {
+}
---
+++ emitter/tcp_emitter_test.go
@@ -0,0 +1,109 @@
+package emitter_test
+
+import (
+	"code.google.com/p/gogoprotobuf/proto"
+	"github.com/cloudfoundry-incubator/dropsonde/emitter"
+	"github.com/cloudfoundry-incubator/dropsonde/events"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+	"io"
+	"net"
+)
+
+func createTestListener() (net.Listener, <-chan *events.Envelope) {
+	listener, _ := net.Listen("tcp", ":0")
+	msgChan := make(chan *events.Envelope)
+
+	go func() {
+		defer close(msgChan)
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				return
+			}
+
+			buf := make([]byte, 4096)
+			n, _ := io.ReadFull(conn, buf)
+
+			envelope := &events.Envelope{}
+			err = proto.Unmarshal(buf[:n], envelope)
+			if err != nil {
+				panic(err)
+			}
+
+			msgChan <- envelope
+		}
+	}()
+
+	return listener, msgChan
+}
+
+var _ = Describe("TcpEmitter", func() {
+	Describe("NewTcpEmitter()", func() {
+		Context("when remoteAddress is parseable", func() {
+			It("returns an emitter", func() {
+				tcpEmitter, err := emitter.NewTcpEmitter("localhost:123")
+				Expect(tcpEmitter).ToNot(BeNil())
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+
+		Context("when remoteAddress is not parseable", func() {
+			It("returns an error", func() {
+				tcpEmitter, err := emitter.NewTcpEmitter("$#&^bad-address!!!:")
+				Expect(tcpEmitter).To(BeNil())
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+
+	Describe("Emit()", func() {
+		var (
+			tcpEmitter emitter.Emitter
+			origin     = &events.Origin{JobName: proto.String("job-name"), JobInstanceId: proto.Int(42)}
+			testEvent  = events.NewHeartbeat(123, 0, 0)
+		)
+
+		Context("when the agent is listening", func() {
+			var (
+				testListener net.Listener
+				msgChan      <-chan *events.Envelope
+			)
+
+			BeforeEach(func() {
+				testListener, msgChan = createTestListener()
+				tcpEmitter, _ = emitter.NewTcpEmitter(testListener.Addr().String())
+				tcpEmitter.SetOrigin(origin)
+			})
+
+			AfterEach(func() {
+				testListener.Close()
+			})
+
+			It("sends events to the agent", func(done Done) {
+				defer close(done)
+
+				err := tcpEmitter.Emit(testEvent)
+				Expect(err).ToNot(HaveOccurred())
+
+				receivedMsg, ok := <-msgChan
+				Expect(ok).To(BeTrue())
+				Expect(receivedMsg.GetOrigin()).To(Equal(origin))
+				Expect(receivedMsg.GetEventType()).To(Equal(events.Envelope_Heartbeat))
+				Expect(receivedMsg.GetHeartbeat()).To(Equal(testEvent))
+			})
+		})
+
+		Context("when the agent is not listening", func() {
+			BeforeEach(func() {
+				tcpEmitter, _ = emitter.NewTcpEmitter("localhost:123")
+				tcpEmitter.SetOrigin(origin)
+			})
+
+			It("returns an error", func() {
+				err := tcpEmitter.Emit(testEvent)
+				Expect(err).To(HaveOccurred())
+			})
+		})
+	})
+})
