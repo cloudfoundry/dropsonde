4be3dd52a8b97f4033debedfb162606f7e26a465
Johannes Tuchscherer <jtuchscherer@pivotallabs.com>
2014-04-04 11:22:08 -0600

    move DefaultEmitter creation into dropsonde.Initialize()
    Signed-off-by: Johannes Petzold <jpetzold@pivotallabs.com>

--- dropsonde.go
+++ dropsonde.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cloudfoundry-incubator/dropsonde/emitter"
 	"github.com/cloudfoundry-incubator/dropsonde/events"
 	"github.com/cloudfoundry-incubator/dropsonde/heartbeat"
+	"log"
 	"sync"
 )
 
@@ -13,9 +14,19 @@ var heartbeatState struct {
 }
 
 func Initialize(origin *events.Origin) {
-	if emitter.DefaultEmitter != nil {
-		emitter.DefaultEmitter.SetOrigin(origin)
+	if emitter.DefaultEmitter == nil {
+		udpEmitter, err := emitter.NewUdpEmitter()
+		if err != nil {
+			log.Fatalf("WARNING: failed to create udpEmitter: %v\n", err)
+		}
+
+		emitter.DefaultEmitter, err = emitter.NewInstrumentedEmitter(udpEmitter)
+		if err != nil {
+			log.Fatalf("WARNING: failed to create instrumentedEmitter: %v\n", err)
+		}
 	}
+
+	emitter.DefaultEmitter.SetOrigin(origin)
 
 	heartbeatState.Lock()
 	defer heartbeatState.Unlock()
--- emitter/emitter.go
+++ emitter/emitter.go
@@ -3,7 +3,6 @@ package emitter
 import (
 	"errors"
 	"github.com/cloudfoundry-incubator/dropsonde/events"
-	"log"
 )
 
 type Emitter interface {
@@ -13,18 +12,6 @@ type Emitter interface {
 }
 
 var DefaultEmitter Emitter
-
-func init() {
-	udpEmitter, err := NewUdpEmitter()
-	if err != nil {
-		log.Printf("WARNING: failed to create udpEmitter: %v\n", err)
-	}
-
-	DefaultEmitter, err = NewInstrumentedEmitter(udpEmitter)
-	if err != nil {
-		log.Printf("WARNING: failed to create instrumentedEmitter: %v\n", err)
-	}
-}
 
 func Emit(e events.Event) error {
 	if DefaultEmitter != nil {
