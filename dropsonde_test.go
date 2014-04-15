e9394f7c1fefbadd9428223341f5e3db5d1ab1b3
John Tuley <jtuley@pivotallabs.com>
2014-04-15 16:48:19 -0600

    Remove dead tests
    Signed-off-by: Alex Jackson <ajackson@pivotallabs.com>

--- dropsonde_test.go
+++ dropsonde_test.go
@@ -5,14 +5,19 @@ import (
 	"github.com/cloudfoundry-incubator/dropsonde/emitter"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	"runtime"
 	"time"
 )
 
 var _ = Describe("Dropsonde", func() {
 	var origin = "awesome-job-name/42"
+	var heartbeatEmitter *emitter.FakeEmitter
 
 	Describe("Initialize", func() {
+		BeforeEach(func() {
+			heartbeatEmitter = emitter.NewFake(origin)
+			emitter.HeartbeatInterval = 10 * time.Millisecond
+		})
+
 		It("errors if passed an origin with empty job name", func() {
 			malformedOriginString := ""
 
@@ -21,59 +26,12 @@ var _ = Describe("Dropsonde", func() {
 			Expect(err.Error()).To(Equal("Cannot initialize dropsonde without an origin"))
 		})
 
-		PIt("creates a DefaultEmitter and starts generating heartbeats", func() {
-			err := dropsonde.Initialize(origin)
-			Expect(err).ToNot(HaveOccurred())
-			emitter.DefaultEmitter = nil
-			runtime.GC()
+		It("Sets the origin information on emitter.DefaultEmitter", func() {
+			fakeEmitter := emitter.NewFake(origin)
+			emitter.DefaultEmitter = fakeEmitter
 
-			//			Eventually(heartbeatEmitter.IsClosed).Should(BeTrue())
-		})
-
-		Context("when there is a preexisting DefaultEmitter", func() {
-			// the existing emitter is overwritten
-		})
-
-		Context("something something", func() {
-			var (
-				heartbeatEmitter *emitter.FakeEmitter
-			)
-
-			BeforeEach(func() {
-				heartbeatEmitter = emitter.NewFake(origin)
-				emitter.HeartbeatInterval = 10 * time.Millisecond
-			})
-
-			It("Sets the origin information on emitter.DefaultEmitter", func() {
-				fakeEmitter := emitter.NewFake(origin)
-				emitter.DefaultEmitter = fakeEmitter
-
-				dropsonde.Initialize(origin)
-				Expect(fakeEmitter.Origin).To(Equal(origin))
-			})
-
-			Context("when the DefaultEmitter is a HeartbeatEventSource", func() {
-				var fakeEmitter *emitter.FakeEmitter
-
-				BeforeEach(func() {
-					fakeEmitter = emitter.NewFake(origin)
-					emitter.DefaultEmitter, _ = emitter.NewInstrumentedEmitter(fakeEmitter)
-				})
-
-				AfterEach(func() {
-					Eventually(heartbeatEmitter.IsClosed).Should(BeTrue())
-				})
-
-				Context("when called for the first time", func() {
-					// Figure out how this has changed
-					PIt("starts the HeartbeatGenerator", func() {
-						dropsonde.Initialize(origin)
-						Expect(heartbeatEmitter.Origin).To(Equal(origin))
-
-						Eventually(func() int { return len(heartbeatEmitter.GetMessages()) }).ShouldNot(BeZero())
-					})
-				})
-			})
+			dropsonde.Initialize(origin)
+			Expect(fakeEmitter.Origin).To(Equal(origin))
 		})
 	})
 })
